45addc02ef5dcd7f52b3f0c12ab68efb65d2fc6b
nikhita <nikhitajamadar1998@gmail.com>
2022-01-10 23:01:49 -0800

    displaying welcome to employee wage computation program

---
+++ EmployeeWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWage
+{
+    class EmployeeWage
+    {
+        public static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to Employee wage computation program");
+        }
+    }
+}
