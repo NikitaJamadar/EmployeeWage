c3a0bca874d8591c0092b178c531edf0a1d746a2
nikhita <nikhitajamadar1998@gmail.com>
2022-01-10 22:56:56 -0800

    main program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage.DisplayWelcome();
+            UC1.CheckAttendance();
+            UC2.DailyEmpWage();
+            UC3.PartTimeWage();
+            UC4.EmpWageUsingSwitchCase();
+            UC5.MonthlyWage();
+            UC6.EmployeeWage();
+        }
+    }
+}
