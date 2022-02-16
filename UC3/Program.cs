0b692fc68a503b96d2b3acef6d0e627557632d31
nikhita <nikhitajamadar1998@gmail.com>
2022-02-16 16:32:54 +0530

    Add part time emp wage

---
+++ UC3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        public static void PartTimeEmpWage()
+        {
+            //Constatnts
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_WAGE_PER_HOUR = 20;
+
+            //Variable
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_WAGE_PER_HOUR;
+            Console.WriteLine("Employee wage is : " + empWage);
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem. ");
+
+            Program.PartTimeEmpWage();
+        }
+    }
+}
