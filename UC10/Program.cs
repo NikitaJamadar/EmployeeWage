bf6bdf7970a07a27d6fbc595b0222e61e47ce520
nikhita <nikhitajamadar1998@gmail.com>
2022-02-22 13:40:52 +0530

    Calculate total wage of each company using array

---
+++ UC10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 5, 10);
+            empWageBuilder.addCompanyEmpWage("TCS", 10, 4, 20);
+            empWageBuilder.ComputeEmpWage();
+            Console.ReadLine();
+        }
+    }
+}
